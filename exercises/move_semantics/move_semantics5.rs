a5599794db3d8065238b197b722f0ca16839b104
Mosio <hesariam@gmail.com>
2023-09-12 19:15:25 +0330

    move_semantics finished

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -6,14 +6,22 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn main() {
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+    let z = &mut x;
     *z += 1000;
     assert_eq!(x, 1200);
 }
+
+
+// The code is attempting to
+// mutate a mutable reference to x (y) and
+// then another mutable reference to x (z).
+// This will result in a compilation error because
+// Rust enforces the rule that you can't have
+// more than one mutable reference to the same data at the same time.
+// This rule ensures memory safety and prevents data races.
